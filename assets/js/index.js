dfb17fb7789aa57b97c8e3243558d3c0607b593e
NicolasFigueroa <nfigueroaxx@gmail.com>
2021-05-13 05:48:43 -0400

    Se añade footer, tooltips y se revisan css y js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,24 @@
+//Función para designar al # que con un click se traslade a la seccion señalada
+//Además se le indica que se realicepaulatinamente asignandole en este caso 900 milisegundos
+$(document).ready(()=>{  
+    $("#").click(function(event){
+        event.preventDefault()
+    let numeral = this.hush;
+    $("html").animate({
+        scrollTop: $(numeral).offset().top
+        }, 
+        900
+        );
+    });
+});
+
+//componente de boostrap para utilizar tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+//componente de boostrap para utilizar popover
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
